82d7a614e1ff9cf2f189f85e5ed60e20f9f79702
Sa4ras <nicesoup1989@protonmail.com>
2020-12-20 00:26:39 +0200

    updated markup and requirements.txt

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+from flask import Flask, redirect, url_for, request, session, render_template, flash
+
+app = Flask(__name__)
+app.secret_key = 'development-key'
+
 if __name__ == '__main__':
-    print('Hello world!')
+    app.run(host='0.0.0.0', debug=True, threaded=True)
 
