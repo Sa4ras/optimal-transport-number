96dd7cb79f81df64d1a9f6cfd718e28901ebddf5
Sa4ras <nicesoup1989@protonmail.com>
2020-12-20 23:26:50 +0200

    updated forms

--- forms/forms.py
+++ forms/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import DecimalField, IntegerField, SubmitField
+from wtforms import DecimalField, IntegerField, SubmitField, StringField, SelectField
 from wtforms.validators import DataRequired, Regexp
 
 import re
@@ -12,24 +12,38 @@ class UserInputForm(FlaskForm):
 
     number_of_vehicles = IntegerField("Кількість транспортних засобів:",
                                       validators=[DataRequired("Будь ласка, уведіть дані"),
-                                                  Regexp('^\d+$', message='Уведіть число без крапки')]
-                                      )
+                                                  Regexp('^\d+$', message='Уведіть число без крапки')])
 
-    route_time = DecimalField("Час повного обороту (гг.хх):",
+    passenger_capacity = IntegerField("Середня пасажиромісткість:",
+                                      validators=[DataRequired("Будь ласка, уведіть дані"),
+                                                  Regexp('^\d+$', message='Уведіть число без крапки')])
+
+    route_time = StringField("Час повного обороту (гг:хх):",
                              validators=[DataRequired("Будь ласка, уведіть дані"),
-                                         Regexp('[0-2][0-9]\:[0-5][0-9]', message='Уведіть коректний час')])
+                                         Regexp('[0-2]?[0-9]\:[0-5][0-9]', message='Уведіть коректний час')])
 
-    first_guard = DecimalField("Початок першої зміни (гг.хх):",
-                               validators=[DataRequired("Будь ласка, уведіть дані"),
-                                           Regexp('^[0-2][0-9]\:[0-5][0-9]$', message='Уведіть коректний час')])
-
-    guard_duration = DecimalField("Довжина зміни (гг.хх):",
-                                  validators=[DataRequired("Будь ласка, уведіть дані"),
-                                              Regexp('^[0-2][0-9]\:[0-5][0-9]$', message='Уведіть коректний час')])
-
-    population = IntegerField("Населення міста:",
+    first_guard = StringField("Початок першої зміни (гг:хх):",
                               validators=[DataRequired("Будь ласка, уведіть дані"),
-                                          Regexp('^\d+$', message='Уведіть число без крапки')])
+                                          Regexp('^[0-2]?[0-9]\:[0-5][0-9]$', message='Уведіть коректний час')])
+
+    guard_duration = SelectField("Довжина зміни:",
+                                 choices=[('1', '1'),
+                                          ('2', '2'),
+                                          ('3', '3'),
+                                          ('4', '4'),
+                                          ('5', '5'),
+                                          ('6', '6'),
+                                          ('7', '7'),
+                                          ('8', '8')],
+                                 validators=[DataRequired("Будь ласка, уведіть дані")])
+
+    number_of_guards = IntegerField("Кількість змін:",
+                                    validators=[DataRequired("Будь ласка, уведіть дані"),
+                                                Regexp('^\d$', message='Уведіть число без крапки')])
+
+    area_population = IntegerField("Населення міста:",
+                                   validators=[DataRequired("Будь ласка, уведіть дані"),
+                                               Regexp('^\d+$', message='Уведіть число без крапки')])
 
     area_square = DecimalField("Площа міста: ",
                                validators=[DataRequired("Будь ласка, уведіть дані"),
