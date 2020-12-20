8d33d95ba78dde43ac3d182fcdc319b1c9dff444
Sa4ras <nicesoup1989@protonmail.com>
2020-12-20 23:47:00 +0200

    updated with MainSolver class

--- main.py
+++ main.py
@@ -1,7 +1,29 @@
 from flask import Flask, redirect, url_for, request, session, render_template, flash
+from forms.forms import UserInputForm
+from helper_class import MainSolver
 
 app = Flask(__name__)
 app.secret_key = 'development-key'
+
+@app.route("/", methods = ['GET'])
+def welcome():
+    return render_template('welcome.html')
+
+@app.route('/calculus', methods=["GET", "POST"])
+def calc():
+    form = UserInputForm()
+    if request.method == 'POST' and form.validate():
+        result = MainSolver(form.route_len,
+                            form.number_of_vehicles,
+                            form.passenger_capacity,
+                            form.route_time,
+                            form.first_guard,
+                            form.guard_duration,
+                            form.number_of_guards,
+                            form.area_population,
+                            form.area_square).optimization_task()
+        print(result)
+    return render_template('calc.html', form=form)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True, threaded=True)
