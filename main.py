18187848942bf74d78fbb3362457f457155f67e7
Sa4ras <nicesoup1989@protonmail.com>
2020-12-21 10:54:10 +0200

    plot added

--- main.py
+++ main.py
@@ -2,6 +2,11 @@ from flask import Flask, request, render_template, flash
 from forms.forms import UserInputForm
 from helper_class import MainSolver
 
+import io
+import base64
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.figure import Figure
+
 app = Flask(__name__)
 app.secret_key = 'development-key'
 
@@ -27,17 +32,27 @@ def calc():
         depart_hours = result[0]
         optimal_transport_number = result[1]
         departured_quantity = result[3]
+
+        fig = Figure()
+        axis = fig.add_subplot(1, 1, 1)
+        axis.set_xlabel("Година відправлення")
+        axis.set_ylabel("Кількість т-з")
+        axis.bar(depart_hours, departured_quantity)
+        pngImage = io.BytesIO()
+        FigureCanvas(fig).print_png(pngImage)
+        pngImageB64String = "data:image/png;base64,"
+        pngImageB64String += base64.b64encode(pngImage.getvalue()).decode('utf8')
+
         if result[2]== 0 and result[4] == True:
             flash(f"""Для Вас має сенс покращити обслуговування маршруту!\
                       Мінімальна потрібна кількість т-з: {optimal_transport_number}\
-                      Рекомендовані початки рейсів та кількість відбутих т-з у цей час: 
-                      {dict(zip(depart_hours,departured_quantity))}""", 'success')
+                      Рекомендовані початки рейсів та кількість відбутих т-з у цей час:""", 'success')
+            return render_template('plot.html', image=pngImageB64String)
         else:
             flash(f"""Для Вас є сенс збільшити автопарк!\
                       Мінімальна потрібна кількість т-з: {optimal_transport_number}\
-                      Рекомендовані початки рейсів та кількість відбутих т-з у цей час: 
-                      {dict(zip(depart_hours,departured_quantity))}""", 'danger')
-
+                      Рекомендовані початки рейсів та кількість відбутих т-з у цей час:""", 'danger')
+            return render_template('plot.html', image=pngImageB64String)
     return render_template('calc.html', form=form)
 
 if __name__ == '__main__':
